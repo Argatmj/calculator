10383b39cb60deef408f45450c775194f229f7dd
Argatmj <ikomangarga0612@gmail.com>
2024-07-17 17:09:04 +0200

    Added clear, delete, and populate-display function

--- main.js
+++ main.js
@@ -31,3 +31,26 @@ function operate(num1, op, num2) {
   }
 }
 
+const calc = document.querySelector(".calc");
+const res = document.querySelector(".result");
+const del = document.querySelector("#del");
+const clear = document.querySelector("#clr");
+let buttons = document.querySelectorAll(".mod");
+
+del.addEventListener("click",() => {
+    let text = calc.textContent;
+    if (text.length > 0) {
+        calc.textContent = text.slice(0, -1); 
+    }
+});
+
+clear.addEventListener("click",() => {
+    res.innerHTML = '';
+    calc.innerHTML = '';
+})
+
+buttons.forEach((button) => button.addEventListener("click", (event) => {
+    let text = calc.innerHTML;
+    calc.innerHTML = text.concat(`${event.target.innerHTML}`); 
+}));
+
