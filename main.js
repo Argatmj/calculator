be9331f6cd8e3a6bc70fca5bdd03ed2b3e9b96d5
Argatmj <ikomangarga0612@gmail.com>
2024-07-17 19:57:00 +0200

    Added operate function to the buttons

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+let flag = false;
+let count = 0;
 
 function add(num1, num2) {
   return num1 + num2;
@@ -24,33 +26,67 @@ function operate(num1, op, num2) {
       return add(num1, num2);
     case "-":
       return subtract(num1, num2);
-    case "*":
+    case "×":
       return multiply(num1, num2);
-    case "/":
+    case "÷":
       return divide(num1, num2);
   }
 }
 
-const calc = document.querySelector(".calc");
-const res = document.querySelector(".result");
+const display = document.querySelector(".display");
+const equal = document.querySelector("#equals")
 const del = document.querySelector("#del");
 const clear = document.querySelector("#clr");
-let buttons = document.querySelectorAll(".mod");
+const numBtns = document.querySelectorAll(".num");
+const opsBtns = document.querySelectorAll(".ops");
 
 del.addEventListener("click",() => {
-    let text = calc.textContent;
+    let text = display.textContent;
     if (text.length > 0) {
-        calc.textContent = text.slice(0, -1); 
+        display.textContent = text.slice(0, -1); 
     }
 });
 
 clear.addEventListener("click",() => {
-    res.innerHTML = '';
-    calc.innerHTML = '';
+    display.innerHTML = '';
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+    flag = false;
 })
 
-buttons.forEach((button) => button.addEventListener("click", (event) => {
-    let text = calc.innerHTML;
-    calc.innerHTML = text.concat(`${event.target.innerHTML}`); 
+equal.addEventListener("click", () => {
+    secondNum = parseFloat(display.innerHTML);
+    console.log(firstNum);
+    console.log(secondNum);
+    console.log(operator);
+    let result = operate(firstNum,operator,secondNum);
+    display.innerHTML = result;
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+    count = 0;
+});
+
+opsBtns.forEach((op) => op.addEventListener("click", (event) => {
+    if(count !== 1){
+        firstNum = parseFloat(display.innerHTML);
+    }else{
+        secondNum = parseFloat(display.innerHTML);
+        firstNum = operate(firstNum,operator,secondNum);
+        secondNum = null;
+        count = 0;
+    }
+    operator = event.target.innerHTML;
+    flag = true;
+    count++;
 }));
 
+numBtns.forEach((button) => button.addEventListener("click", (event) => {
+    let text = display.innerHTML;
+    if(flag){
+        text = '';
+        flag = false;
+    }
+    display.innerHTML = text.concat(`${event.target.innerHTML}`); 
+}));
